cb303efa272af0f6defa5efbd66095edce8cfa6a
djoker-koker <calab0k@yandex.ru>
2025-12-16 03:57:58 +0300

    Create test_shell_sort.cpp

---
+++ sem3/tests/test_shell_sort.cpp
@@ -0,0 +1,38 @@
+#include "../../catch_amalgamated.hpp"
+#include "../sorting/include/shell_sort.h"
+
+TEST_CASE("Domashka")
+{
+    std::vector<int> arr = {17, 3, 0, 12, 6, 9, 19, 1};
+    std::vector<int> expected = {0, 1, 3, 6, 9, 12, 17, 19};
+
+    shell_sort(arr);
+    REQUIRE(arr == expected);
+}
+
+TEST_CASE("Pustoy massiv")
+{
+    std::vector<int> arr = {};
+    std::vector<int> expected = {};
+
+    shell_sort(arr);
+    REQUIRE(arr == expected);
+}
+
+TEST_CASE("Odin element")
+{
+    std::vector<int> arr = {5};
+    std::vector<int> expected = {5};
+
+    shell_sort(arr);
+    REQUIRE(arr == expected);
+}
+
+TEST_CASE("Otsortirovano")
+{
+    std::vector<int> arr = {1, 2, 3, 4, 5};
+    std::vector<int> expected = {1, 2, 3, 4, 5};
+
+    shell_sort(arr);
+    REQUIRE(arr == expected);
+}
